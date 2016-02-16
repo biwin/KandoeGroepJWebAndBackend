7cbe8a3b37dcf0140465da9793083cd61218b65c
Jan <jan.somers@student.kdg.be>
2016-02-16 12:05:59 +0100

    ok

---
+++ app/backend/dao/userDao.js
@@ -0,0 +1,23 @@
+/// <reference path="../../../typings/mongoose/mongoose.d.ts" />
+var mongoose_1 = require("mongoose");
+var daoConstants_1 = require("./daoConstants");
+var UserDao = (function () {
+    function UserDao() {
+        this._db = new mongoose_1.Mongoose().connect(daoConstants_1.DaoConstants.CONNECTION_URL);
+        this._userSchema = new mongoose.Schema({
+            _id: String,
+            _email: String,
+            _password: String
+        }, { versionKey: false });
+        this._userModel = mongoose.model('User', _userSchema);
+    }
+    UserDao.prototype.create = function (id, email, password) {
+        var user = new this._userModel({ _id: id, _email: email, _password: password });
+        user.save(function (createdUser) {
+            return createdUser;
+        });
+    };
+    return UserDao;
+})();
+exports.UserDao = UserDao;
+//# sourceMappingURL=userDao.js.map
