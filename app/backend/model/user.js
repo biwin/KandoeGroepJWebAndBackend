fc38430719932ad2ec58e3e5565e7796843d5cd4
Jasper Catthoor <jasper@catthoor.com>
2016-02-16 15:25:30 +0100

    :D

--- app/backend/model/user.js
+++ app/backend/model/user.js
@@ -3,4 +3,4 @@ var mongoose_2 = require("mongoose");
 var userSchema = new mongoose_1.Schema({ email: String, password: String, displayName: String });
 var User = mongoose_2.model("User", userSchema);
 module.exports = User;
-//# sourceMappingURL=User.js.map
+//# sourceMappingURL=user.js.map
