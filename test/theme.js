cf24d7b80141fbdd9782510d4ae512eb97fc75bb
Jasper Catthoor <jasper@catthoor.com>
2016-02-17 16:59:33 +0100

    Fixed test

--- test/theme.js
+++ test/theme.js
@@ -6,14 +6,17 @@ var themeDao_1 = require("../app/backend/dao/themeDao");
 describe('Theme', function () {
     describe('#addTheme', function () {
         it('theme must be saved in the database', function (done) {
-            var themeDao = new themeDao_1.ThemeDao();
-            var theme = new theme_1.Theme(1, "Bars", "Bars we want to visit");
-            themeDao.create(theme, function () {
-                themeDao.read("Bars", function (t) {
-                    assert.equal(t._name, theme._name);
-                    done();
+            this.timeout(0);
+            setTimeout(function () {
+                var themeDao = new themeDao_1.ThemeDao();
+                var theme = new theme_1.Theme(1, "Bars", "Bars we want to visit");
+                themeDao.create(theme, function () {
+                    themeDao.read("Bars", function (t) {
+                        assert.equal(t._name, theme._name);
+                        done();
+                    });
                 });
-            });
+            }, 3000);
         });
     });
 });
--- test/theme.ts
+++ test/theme.ts
@@ -3,20 +3,22 @@
 import assert = require('assert');
 import {Theme} from "../app/backend/model/theme";
 import {ThemeDao} from "../app/backend/dao/themeDao";
+import {timeout} from "rxjs/operator/timeout";
 
 describe('Theme', () => {
     describe('#addTheme', () => {
-        it('theme must be saved in the database', (done:any) =>{
-            var themeDao:ThemeDao = new ThemeDao();
-            var theme:Theme = new Theme(1,"Bars","Bars we want to visit");
-            themeDao.create(theme, () => {
-                themeDao.read("Bars", (t:Theme) => {
-                    assert.equal(t._name, theme._name);
-                    done();
+        it('theme must be saved in the database', function(done: any) {
+            this.timeout(0);
+            setTimeout(() => {
+                var themeDao:ThemeDao = new ThemeDao();
+                var theme:Theme = new Theme(1,"Bars","Bars we want to visit");
+                themeDao.create(theme, () => {
+                    themeDao.read("Bars", (t:Theme) => {
+                        assert.equal(t._name, theme._name);
+                        done();
+                    });
                 });
-            });
-
+            }, 3000);
         });
     });
 });
-
