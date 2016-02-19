c772e4147a65d7c33c700cac52a841bc57e23795
Jasper Catthoor <jasper@catthoor.com>
2016-02-19 14:31:07 +0100

    ok

---
+++ app/backend/logic/themeManager.js
@@ -0,0 +1,33 @@
+var themeDao_1 = require("../dao/themeDao");
+var ThemeManager = (function () {
+    function ThemeManager() {
+        this._dao = new themeDao_1.ThemeDao();
+    }
+    ThemeManager.prototype.clearDatabase = function (callback) {
+        this._dao.clearDatabase(callback);
+    };
+    ThemeManager.prototype.createTheme = function (theme, callback) {
+        var _this = this;
+        this.themeExists(theme._name, function (exists) {
+            if (exists) {
+                callback(null);
+            }
+            else {
+                _this._dao.createTheme(theme, function () {
+                    _this.getTheme(theme._name, callback);
+                });
+            }
+        });
+    };
+    ThemeManager.prototype.getTheme = function (name, callback) {
+        this._dao.readTheme(name, callback);
+    };
+    ThemeManager.prototype.themeExists = function (name, callback) {
+        this.getTheme(name, function (t) {
+            callback(t != null);
+        });
+    };
+    return ThemeManager;
+})();
+exports.ThemeManager = ThemeManager;
+//# sourceMappingURL=themeManager.js.map
---
+++ app/backend/model/organisation.js
@@ -0,0 +1,9 @@
+var Organisation = (function () {
+    function Organisation(_name, _organisators) {
+        this._name = _name;
+        this._organisators = _organisators;
+    }
+    return Organisation;
+})();
+exports.Organisation = Organisation;
+//# sourceMappingURL=organisation.js.map
