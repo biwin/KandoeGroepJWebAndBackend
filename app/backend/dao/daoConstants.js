620c7d70cdec98aca2856668de90837912420366
Jasper Catthoor <jasper@catthoor.com>
2016-02-22 15:16:07 +0100

    fuck jenkins

--- app/backend/dao/daoConstants.js
+++ app/backend/dao/daoConstants.js
@@ -1,4 +1,14 @@
-/**
- * Created by Jasper on 22/02/2016.
- */
+var DaoConstants = (function () {
+    function DaoConstants() {
+    }
+    Object.defineProperty(DaoConstants, "CONNECTION_URL", {
+        get: function () {
+            return "mongodb://KandoeNodeUser:omgenio1234@ds055895.mongolab.com:55895/kandoe";
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return DaoConstants;
+})();
+exports.DaoConstants = DaoConstants;
 //# sourceMappingURL=daoConstants.js.map
