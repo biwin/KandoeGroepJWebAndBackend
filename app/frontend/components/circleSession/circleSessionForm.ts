d9f98d0a94483f265e552287378adc5f5c029767
Michaël De Boey <info@michaeldeboey.be>
2016-02-26 13:51:31 +0100

    Added form into a card

--- app/frontend/components/circleSession/circleSessionForm.ts
+++ app/frontend/components/circleSession/circleSessionForm.ts
@@ -1,6 +1,6 @@
 import {Component} from "angular2/core";
-import {Input} from "angular2/core";
-import {AfterViewInit} from "angular2/core";
+import {Input, AfterViewInit} from "angular2/core";
+
 import {Group} from "../../../backend/model/group";
 import {Theme} from "../../../backend/model/theme";
 
@@ -9,53 +9,57 @@ import {Theme} from "../../../backend/model/theme";
     selector: 'circlesession-form',
     template: `
     <div class="row container">
-    <h5>Nieuw Spel</h5>
-    <form (submit)="OnSubmit()" class="col s12">
-      <div class="row">
-        <div class="input-field col s3">
-            <select>
-              <option value="" disabled selected>Groep</option>
-              <option *ngFor="#group of _groups" value="{{group._name}}">{{group._name}}</option>
-            </select>
-            <label>Groep</label>
-        </div>
-        <div class="input-field col s3">
-            <select>
-              <option value="" disabled selected>Thema</option>
-              <option *ngFor="#theme of _themes" value="{{theme._name}}">{{theme._name}}</option>
-            </select>
-            <label>Thema</label>
-        </div>
+        <h5>Nieuw Spel</h5>
 
-      </div>
-    <div class="row">
-    <div class="col s5">
-      <div class="switch">
-        <label>
-          Uitgesteld
-          <input type="checkbox">
-          <span class="lever"></span>
-          Realtime
-        </label>
-      </div>
-     </div>
+        <div class="card formCard"><div class="card-content">
+            <form (submit)="OnSubmit()" class="col s12">
+                <div class="row">
+                    <div class="input-field col s3">
+                        <select>
+                            <option value="" disabled selected>Groep</option>
+                            <option *ngFor="#group of _groups" value="{{group._name}}">{{group._name}}</option>
+                        </select>
+
+                        <label>Groep</label>
+                    </div>
+
+                    <div class="input-field col s3">
+                        <select>
+                            <option value="" disabled selected>Thema</option>
+                            <option *ngFor="#theme of _themes" value="{{theme._name}}">{{theme._name}}</option>
+                        </select>
+
+                        <label>Thema</label>
+                    </div>
+                </div>
+
+                <div class="row"><div class="col s5">
+                    <div class="switch">
+                        <label>
+                            Uitgesteld
+                            <input type="checkbox">
+                            <span class="lever"></span>
+                            Realtime
+                        </label>
+                    </div>
+                </div></div>
+
+                <div class="row">
+                    <div class="col">
+                        <label>Start datum</label>
+                        <input type="date" class="datepicker">
+                    </div>
+
+                    <div class="input-field col s6">
+                        <input id="time" type="text" class="validate">
+                        <label for="time">Beginuur</label>
+                    </div>
+                </div>
+
+                <button type="submit" class="waves-effect waves-light btn red"><i class="material-icons center">add</i></button>
+            </form>
+        </div></div>
     </div>
-
-    <div class="row">
-    <div class="col">
-         <label>Start datum</label>
-         <input type="date" class="datepicker">
-    </div>
-     <div class="input-field col s6">
-          <input id="time" type="text" class="validate">
-          <label for="time">Beginuur</label>
-     </div>
-    </div>
-
-
-      <button type="submit" class="waves-effect waves-light btn red"><i class="material-icons center">add</i></button>
-    </form>
-  </div>
     `
 })
 export class CircleSessionForm implements AfterViewInit {
@@ -74,5 +78,3 @@ export class CircleSessionForm implements AfterViewInit {
         });
     }
 }
-
-
