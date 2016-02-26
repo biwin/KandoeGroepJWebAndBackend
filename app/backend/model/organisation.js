ad577781174a4f9543fae490f9c26a479f96cbf2
Michaël De Boey <info@michaeldeboey.be>
2016-02-26 02:27:26 +0100

    Added attribute _members: User[]

--- app/backend/model/organisation.js
+++ app/backend/model/organisation.js
@@ -1,12 +1,27 @@
+var user_1 = require("./user");
 var Organisation = (function () {
     function Organisation(_name, _memberIds) {
         this._name = _name;
         this._memberIds = _memberIds;
         this._groups = [];
+        this._members = [];
+        for (var i = 0; i < this._memberIds.length; i++) {
+            var organisatorId = this._memberIds[i];
+            var newUser = user_1.User.empty();
+            newUser._name = organisatorId;
+            this._members.push(newUser);
+        }
     }
     Object.defineProperty(Organisation.prototype, "groups", {
         get: function () {
             return this._groups;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Organisation.prototype, "members", {
+        get: function () {
+            return this._members;
         },
         enumerable: true,
         configurable: true
--- app/backend/model/organisation.ts
+++ app/backend/model/organisation.ts
@@ -1,17 +1,33 @@
 import {Group} from "./group";
+import {User} from "./user";
 
 export class Organisation {
     private _groups: Group[];
+    private _members: User[];
 
     public _id: string;
 
     constructor(public _name: string,
                 public _memberIds: string[]) {
         this._groups = [];
+        this._members = [];
+
+        for (var i = 0; i < this._memberIds.length; i++) {
+            var organisatorId = this._memberIds[i];
+            var newUser: User = User.empty();
+
+            newUser._name = organisatorId;
+
+            this._members.push(newUser);
+        }
     }
 
     get groups(): Group[] {
         return this._groups;
+    }
+
+    get members():User[] {
+        return this._members;
     }
 
     public static empty(): Organisation {
