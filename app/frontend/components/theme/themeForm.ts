bcf521c479e7d235459fd8b80c93c392867c187d
Michaël De Boey <info@michaeldeboey.be>
2016-02-26 13:43:34 +0100

    Added form into a card

--- app/frontend/components/theme/themeForm.ts
+++ app/frontend/components/theme/themeForm.ts
@@ -8,26 +8,27 @@ import {TagInput} from "../general/tagInput";
     selector: 'theme-form',
     template: `
     <div class="row container">
-    <h5>Nieuw Thema</h5>
-    <form (submit)="OnSubmit()" class="col s12">
-      <div class="row">
-        <div class="input-field col s6">
-          <input [(ngModel)]="theme._name" id="name" type="text">
-          <label for="name">Naam</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input [(ngModel)]="theme._description" id="description" type="text">
-          <label for="description">Beschrijving</label>
-        </div>
-      </div>
-      <div class="row">
-        <tags [title]="'Tags (end each tag with a semicolon)'" [tagArray]="theme._tags"></tags>
-      </div>
-      <button type="submit" class="waves-effect waves-light btn red"><i class="material-icons center">add</i></button>
-    </form>
-  </div>
+        <h5>Nieuw Thema</h5>
+        <div class="card formCard"><div class="card-content">
+            <form (submit)="OnSubmit()" class="col s12">
+                <div class="row"><div class="input-field col s6">
+                    <input [(ngModel)]="theme._name" id="name" type="text">
+                    <label for="name">Naam</label>
+                </div></div>
+
+                <div class="row"><div class="input-field col s12">
+                    <input [(ngModel)]="theme._description" id="description" type="text">
+                    <label for="description">Beschrijving</label>
+                </div></div>
+
+                <div class="row">
+                    <tags [title]="'Tags (end each tag with a semicolon)'" [tagArray]="theme._tags"></tags>
+                </div>
+
+                <button type="submit" class="waves-effect waves-light btn red"><i class="material-icons center">add</i></button>
+            </form>
+        </div></div>
+    </div>
     `,
     directives: [CORE_DIRECTIVES, FORM_DIRECTIVES, TagInput]
 })
@@ -35,7 +36,8 @@ import {TagInput} from "../general/tagInput";
 export class ThemeForm {
     theme:Theme = Theme.empty();
     private OnSubmit(){
-        this.theme._organisatorIds = ["CURRENT_USER_ID"]
+        this.theme._organisatorIds = ["CURRENT_USER_ID"];
+
         //TODO: call backend
         alert(this.theme._name + "  " + this.theme._description);
         alert(this.theme._tags);
