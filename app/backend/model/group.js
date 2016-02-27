f6f2c1f756aa2a9eae4f41e7eedb1502ecdfd04f
Michaël De Boey <info@michaeldeboey.be>
2016-02-28 00:55:00 +0100

    Added get members: User[] method

--- app/backend/model/group.js
+++ app/backend/model/group.js
@@ -1,3 +1,4 @@
+var user_1 = require("./user");
 var Group = (function () {
     function Group(_name, _description, _organisationId, _memberIds) {
         this._name = _name;
@@ -5,6 +6,23 @@ var Group = (function () {
         this._organisationId = _organisationId;
         this._memberIds = _memberIds;
     }
+    Object.defineProperty(Group.prototype, "members", {
+        get: function () {
+            //TODO: call backend
+            var members = [];
+            if (this._memberIds) {
+                for (var i = 0; i < this._memberIds.length; i++) {
+                    var memberId = this._memberIds[i];
+                    var newUser = user_1.User.empty();
+                    newUser._name = memberId;
+                    members.push(newUser);
+                }
+            }
+            return members;
+        },
+        enumerable: true,
+        configurable: true
+    });
     Group.empty = function () {
         return new Group("", "", "", []);
     };
--- app/backend/model/group.ts
+++ app/backend/model/group.ts
@@ -1,3 +1,5 @@
+import {User} from "./user";
+
 export class Group {
     public _id: string;
 
@@ -6,6 +8,24 @@ export class Group {
                 public _organisationId: string,
                 public _memberIds: string[]) {
 
+    }
+
+    get members(): User[] {
+        //TODO: call backend
+        var members: User[] = [];
+
+        if(this._memberIds) {
+            for (var i = 0; i < this._memberIds.length; i++) {
+                var memberId = this._memberIds[i];
+                var newUser: User = User.empty();
+
+                newUser._name = memberId;
+
+                members.push(newUser);
+            }
+        }
+
+        return members;
     }
 
     public static empty(): Group{
