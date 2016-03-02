21f9547931444f03bc0ccd58fa37d05b1bb8ad93
Rob Hendrickx <chat-rob@hotmail.com>
2016-03-02 18:34:08 +0100

    Visualisatie kaarten op bord

--- app/frontend/components/circleSession/circleSessionCardDetail.ts
+++ app/frontend/components/circleSession/circleSessionCardDetail.ts
@@ -1,20 +1,27 @@
-import {Component} from "angular2/core";
+import {Component, Inject, Output, Input, EventEmitter} from "angular2/core";
 import {CORE_DIRECTIVES} from "angular2/common";
-import {Inject} from "angular2/core";
 import {CircleSessionService} from "../../services/circleSessionService";
 import {CircleSession} from "../../../backend/model/circleSession";
 import {CircleSessionCard} from "./circleSessionCard";
 import {RouteParams} from "angular2/router";
-import {Input} from "angular2/core";
 import {Card} from "../../../backend/model/card";
 
 @Component({
     selector: 'circlesession-card',
     template: `
         <div class="col s3">
-      <div (click)="openCard()" class="card hoverable">
-        <div class="card-content">
-           <span class="card-title">{{card._name}} <a class="btn-floating btn waves-effect waves-light blue right"><i class="material-icons">arrow_upward</i></a></span>
+      <div class="card hoverable" [style.background]="color" (mouseenter)="onMouseEnter()" (mouseleave)="onMouseLeave()">
+        <div class="card-content container">
+            <div class="row">
+                <div class="col s12 center">
+                  <a class="btn-floating btn waves-effect waves-light blue"><i class="material-icons">arrow_upward</i></a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col s12 center">
+                    {{card._name}}
+                </div>
+            </div>
         </div>
       </div>
       </div>
@@ -25,4 +32,14 @@ import {Card} from "../../../backend/model/card";
 
 export class CircleSessionCardDetail {
     @Input() private card:Card;
+    @Input() private color:string = "";
+    @Output() private hover:EventEmitter<boolean> = new EventEmitter();
+
+    onMouseEnter() {
+        this.hover.emit(true);
+    }
+
+    onMouseLeave() {
+        this.hover.emit(false);
+    }
 }
--- app/frontend/components/circleSession/circleSessionGame.ts
+++ app/frontend/components/circleSession/circleSessionGame.ts
@@ -1,6 +1,5 @@
-import {Component} from "angular2/core";
+import {Component, Inject} from "angular2/core";
 import {CORE_DIRECTIVES} from "angular2/common";
-import {Inject} from "angular2/core";
 import {CircleSessionService} from "../../services/circleSessionService";
 import {CircleSession} from "../../../backend/model/circleSession";
 import {CircleSessionCard} from "./circleSessionCard";
@@ -8,17 +7,35 @@ import {RouteParams} from "angular2/router";
 import {Card} from "../../../backend/model/card";
 import {ThemeService} from "../../services/themeService";
 import {CircleSessionCardDetail} from "./circleSessionCardDetail";
+import {CircleSessionConstants} from "./../../logic/circleSessionConstants";
+import {CardPosition} from "../../../backend/model/cardPosition";
 
 @Component({
     selector: 'circlesession-game',
     template: `
     <div class="container">
         <div class="row margin-top">
-            <div class="col s12 kandoeCircle">
+            <div class="col s8 offset-s2">
+                <svg [attr.viewBox]="constants.VIEWBOX">
+                    <!-- Draw Kandoe board circles -->
+                    <circle *ngFor="#filled of constants.RINGS; #i = index"
+                            [attr.r]="constants.CircleRadius(i+1)"
+                            [attr.stroke-width]="constants.RING_WIDTH"
+                            [attr.cy]="constants.CENTER" [attr.cx]="constants.CENTER"
+                            id="circle-{{i+1}}" class="kandoeRing" [class.inner]="filled"/>
+
+
+                    <!-- circle voorbeelden vervangen door cardposition -->
+                    <circle *ngFor="#bol of pst; #i = index"
+                            [id]="bol._cardId"
+                            [attr.r]="35" [attr.fill]="hoveredCardId === bol._cardId ? constants.HOVERED_COLOR : constants.CardColor(i)" [attr.cy]="constants.YPOS_CIRCLE(bol._position, (1 / pst.length) * i)"
+                            [attr.cx]="constants.XPOS_CIRCLE(bol._position, (1 / pst.length) * i)" stroke-width="2" [attr.stroke]="constants.CARD_STROKE_COLOR"/>
+                </svg>
             </div>
         </div>
+
         <div class="row">
-            <circlesession-card *ngFor="#card of cards" [card]="card"></circlesession-card>
+            <circlesession-card *ngFor="#card of cards; #i = index" [card]="card" [color]="constants.CardColor(i)" (hover)="hover(card._id, $event)"></circlesession-card>
         </div>
     </div>
     `,
@@ -26,9 +43,12 @@ import {CircleSessionCardDetail} from "./circleSessionCardDetail";
 })
 
 export class CircleSessionGame {
+    private constants:CircleSessionConstants = new CircleSessionConstants();
     private circleSession:CircleSession = CircleSession.empty();
     private cards:Card[] = [];
+    private pst:CardPosition[] = [];
     private id:string;
+    private hoveredCardId:string = "";
 
     constructor(service:CircleSessionService,themeService:ThemeService ,route:RouteParams) {
         this.id = route.get('id');
@@ -36,8 +56,19 @@ export class CircleSessionGame {
         service.get(this.id).subscribe((circleSession:CircleSession) => {
             this.circleSession = circleSession;
             themeService.getCards(circleSession._themeId).subscribe((cs:Card[]) => {
-                cs.forEach((c:Card) => this.cards.push(c));
+                cs.forEach((c:Card) => {
+                    this.cards.push(c);
+                    this.pst.push(new CardPosition(this.id, c._id, "", Math.floor(Math.random() * 5) + 1, new Date()));
+                });
             })
         });
+    }
+
+    hover(id:string, mouseover:boolean) {
+        if(mouseover) {
+            this.hoveredCardId = id;
+        } else {
+            this.hoveredCardId = "";
+        }
     }
 }
