5cbb910c311d38e2dd0334e730896d69b17114fd
Michaël De Boey <info@michaeldeboey.be>
2016-03-02 19:46:19 +0100

    Added .js & .js.map files to repository

---
+++ app/frontend/logic/circleMathToolbox.js
@@ -0,0 +1,16 @@
+var CircleMathToolbox = (function () {
+    function CircleMathToolbox() {
+    }
+    CircleMathToolbox.POS_ON_CIRCLE = function (center, r, angle, f) {
+        var pos = center;
+        pos += r;
+        pos *= f(CircleMathToolbox.DEG_TO_RAD(angle));
+        return pos;
+    };
+    CircleMathToolbox.DEG_TO_RAD = function (deg) {
+        return deg * Math.PI / 180;
+    };
+    return CircleMathToolbox;
+})();
+exports.CircleMathToolbox = CircleMathToolbox;
+//# sourceMappingURL=circleMathToolbox.js.map
