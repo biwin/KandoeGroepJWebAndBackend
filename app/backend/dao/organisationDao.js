6a5324816a8da86b967430148a4a8a5a5732e1ca
Michaël De Boey <info@michaeldeboey.be>
2016-03-02 17:22:27 +0100

    Added implementation for createOrganisation

--- app/backend/dao/organisationDao.js
+++ app/backend/dao/organisationDao.js
@@ -1,7 +1,21 @@
+/// <reference path="../../../typings/mongodb/mongodb.d.ts" />
+var mongodb_1 = require("mongodb");
+var daoConstants_1 = require("./daoConstants");
 var OrganisationDao = (function () {
     function OrganisationDao() {
+        this._client = new mongodb_1.MongoClient();
     }
     OrganisationDao.prototype.createOrganisation = function (organisation, callback) {
+        this._client.connect(daoConstants_1.DaoConstants.CONNECTION_URL, function (err, db) {
+            db.collection('organisations').insertOne(organisation, function (error, result) {
+                if (error != null) {
+                    console.log(error.message);
+                }
+                organisation._id = result.insertedId;
+                db.close();
+                callback(organisation);
+            });
+        });
     };
     OrganisationDao.prototype.readOrganisationByName = function (organisationName, callback) {
     };
--- app/backend/dao/organisationDao.ts
+++ app/backend/dao/organisationDao.ts
@@ -1,8 +1,32 @@
+/// <reference path="../../../typings/mongodb/mongodb.d.ts" />
+
+import {MongoClient, Db, MongoError} from "mongodb";
+
+import {DaoConstants} from "./daoConstants";
+
 import {Organisation} from "../model/organisation";
 
 export class OrganisationDao {
-    createOrganisation(organisation: Organisation, callback: (newOrganisation: Organisation) => any) {
+    private _client: MongoClient;
 
+    constructor() {
+        this._client = new MongoClient();
+    }
+
+    createOrganisation(organisation: Organisation, callback: (newOrganisation: Organisation) => any) {
+        this._client.connect(DaoConstants.CONNECTION_URL, (err: any, db: Db) => {
+            db.collection('organisations').insertOne(organisation, (error: MongoError, result) => {
+                if (error != null) {
+                    console.log(error.message);
+                }
+
+                organisation._id = result.insertedId;
+
+                db.close();
+
+                callback(organisation);
+            });
+        });
     }
 
     readOrganisationByName(organisationName: string, callback: (organisation: Organisation) => any) {
