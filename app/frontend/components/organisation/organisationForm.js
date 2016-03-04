f32e448534d78e42b17c01ae3abb80add4ed6810
Michaël De Boey <info@michaeldeboey.be>
2016-03-04 15:58:21 +0100

    Set correct userId

--- app/frontend/components/organisation/organisationForm.js
+++ app/frontend/components/organisation/organisationForm.js
@@ -12,18 +12,21 @@ var common_1 = require("angular2/common");
 var router_1 = require("angular2/router");
 var organisationService_1 = require("../../services/organisationService");
 var organisation_1 = require("../../../backend/model/organisation");
+var userService_1 = require("../../services/userService");
 var OrganisationForm = (function () {
-    function OrganisationForm(router, service) {
+    function OrganisationForm(router, organisationService, userService) {
         this.organisation = organisation_1.Organisation.empty();
         this.router = router;
-        this.service = service;
+        this.organisationService = organisationService;
+        this.userService = userService;
     }
     OrganisationForm.prototype.OnSubmit = function () {
         var _this = this;
-        //TODO: set correct userID
-        this.organisation._organisatorIds.push("CURRENT_USER_ID");
-        this.service.createOrganisation(this.organisation).subscribe(function (o) {
-            _this.router.navigate(["/OrganisationDetail", { id: o._id }]);
+        this.userService.getUserId(function (userId) {
+            _this.organisation._organisatorIds.push(userId);
+            _this.organisationService.createOrganisation(_this.organisation).subscribe(function (o) {
+                _this.router.navigate(["/OrganisationDetail", { id: o._id }]);
+            });
         });
     };
     OrganisationForm = __decorate([
@@ -32,7 +35,7 @@ var OrganisationForm = (function () {
             template: "\n    <div class=\"row container\">\n        <h5>Maak nieuwe organisatie aan</h5>\n\n        <div class=\"card formCard\"><div class=\"card-content\">\n            <form (submit)=\"OnSubmit()\" class=\"col s12\">\n                <div class=\"row\"><div class=\"input-field col s6\">\n                    <input [(ngModel)]=\"organisation._name\" id=\"name\" type=\"text\">\n                    <label for=\"name\">Naam</label>\n                </div></div>\n\n                <button type=\"submit\" class=\"waves-effect waves-light btn red\"><i class=\"material-icons center\">add</i></button>\n            </form>\n        </div></div>\n    </div>\n    ",
             directives: [common_1.CORE_DIRECTIVES, common_1.FORM_DIRECTIVES]
         }), 
-        __metadata('design:paramtypes', [router_1.Router, organisationService_1.OrganisationService])
+        __metadata('design:paramtypes', [router_1.Router, organisationService_1.OrganisationService, userService_1.UserService])
     ], OrganisationForm);
     return OrganisationForm;
 })();
