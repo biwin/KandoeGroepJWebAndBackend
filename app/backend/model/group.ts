0994871101caef1d6f496357ddd670c983ef933f
Michaël De Boey <info@michaeldeboey.be>
2016-03-11 20:13:06 +0100

    Removed unnecessary properties

--- app/backend/model/group.ts
+++ app/backend/model/group.ts
@@ -11,33 +11,6 @@ export class Group {
 
     }
 
-    public getMembers(): User[] {
-        //TODO: call backend
-        var members: User[] = [];
-
-        if(this._memberIds) {
-            for (var i = 0; i < this._memberIds.length; i++) {
-                var memberId = this._memberIds[i];
-                var newUser: User = User.empty();
-
-                newUser._name = memberId;
-
-                members.push(newUser);
-            }
-        }
-
-        return members;
-    }
-
-    public getOrganisation(): Organisation {
-        //TODO: call backend
-        var organisation: Organisation = Organisation.empty();
-
-        organisation._name = this._organisationId;
-
-        return organisation;
-    }
-
     public static empty(): Group{
         return new Group("", "", "", []);
     }
