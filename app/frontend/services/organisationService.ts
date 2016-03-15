d765bf62a10f7807a72a2eaed5b228a505c95664
Michaël De Boey <info@michaeldeboey.be>
2016-03-15 11:27:28 +0100

    Changed standard http to new HttpWrapperService

--- app/frontend/services/organisationService.ts
+++ app/frontend/services/organisationService.ts
@@ -2,37 +2,44 @@ import {Injectable, Inject} from "angular2/core";
 import {Http, Response} from "angular2/http";
 import {Observable} from "rxjs/Observable";
 
+import {HttpWrapperService} from "./httpWrapperService";
+
 import {Organisation} from "../../backend/model/organisation";
 import {Group} from "../../backend/model/group";
 import {User} from "../../backend/model/user";
 
 @Injectable()
 export class OrganisationService {
-    private http: Http = null;
+    private http: HttpWrapperService = null;
     private path: string;
 
-    constructor(http: Http, @Inject("App.BackendPath") path: string) {
+    constructor(http: HttpWrapperService, @Inject("App.BackendPath") path: string) {
         this.path = path;
         this.http = http;
     }
 
     createOrganisation(organisation: Organisation): Observable<Organisation> {
-        return this.http.post(this.path + "organisations/", JSON.stringify(organisation)).map((res: Response) => res.json());
+        return this.http.post(this.path + "organisations", JSON.stringify(organisation), true, true, false);
     }
 
     getOrganisationById(organisationId: string): Observable<Organisation> {
-        return this.http.get(this.path + "organisations/" + organisationId).map((res: Response) => res.json());
+        return this.http.get(this.path + "organisations/" + organisationId, false, true, false);
+    }
+
+    getAdminsOfOrganisationById(organisationId: string): Observable<User[]> {
+        return this.http.get(this.path + "organisations/" + organisationId + "/admins", false, true, false);
     }
 
     getGroupsOfOrganisationById(organisationId: string): Observable<Group[]> {
-        return this.http.get(this.path + "organisations/" + organisationId + "/groups").map((res: Response) => res.json());
+        return this.http.get(this.path + "organisations/" + organisationId + "/groups", false, true, false);
+
     }
 
     getMembersOfOrganisationById(organisationId: string): Observable<User[]> {
-        return this.http.get(this.path + "organisations/" + organisationId + "/members").map((res: Response) => res.json());
+        return this.http.get(this.path + "organisations/" + organisationId + "/members", false, true, false);
     }
 
     deleteMemberFromOrganisationById(userId: string, organisationId: string) {
-        return this.http.delete(this.path + "organisations/" + organisationId + "/members/" + userId);
+        return this.http.delete(this.path + "organisations/" + organisationId + "/members/" + userId, false, false, false);
     }
 }
