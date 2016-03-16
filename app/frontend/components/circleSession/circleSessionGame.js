d99ab227fad888fb4162f217eb2cb3da2ad253f3
Rob Hendrickx <chat-rob@hotmail.com>
2016-03-16 16:05:49 +0100

    Socket fix for deployment? v3

--- app/frontend/components/circleSession/circleSessionGame.js
+++ app/frontend/components/circleSession/circleSessionGame.js
@@ -7,6 +7,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 var core_1 = require("angular2/core");
 var common_1 = require("angular2/common");
 var circleSessionService_1 = require("../../services/circleSessionService");
@@ -21,7 +24,7 @@ var circleSessionCardOnBoardPipe_1 = require("../../logic/circleSessionCardOnBoa
 var core_2 = require("angular2/core");
 var common_2 = require("angular2/common");
 var CircleSessionGame = (function () {
-    function CircleSessionGame(service, themeService, route) {
+    function CircleSessionGame(service, themeService, socketUrl, route) {
         var _this = this;
         this.constants = new circleSessionConstants_1.CircleSessionConstants();
         this.circleSession = circleSession_1.CircleSession.empty();
@@ -35,7 +38,7 @@ var CircleSessionGame = (function () {
             _this.circleSession = circleSession;
             /*SOCKET UPDATE*/
             _this.zone = new core_2.NgZone({ enableLongStackTrace: false });
-            _this.socket = io("http://localhost:8000");
+            _this.socket = io(socketUrl);
             _this.socket.emit('join session', JSON.stringify({ sessionId: _this.circleSession._id || 'Unknown' }));
             _this.socket.on('send move', function (data) { return _this.zone.run(function () {
                 var dataObject = JSON.parse(data);
@@ -91,8 +94,9 @@ var CircleSessionGame = (function () {
             template: "\n    <div class=\"padding-right-users\">\n        <div class=\"row\">\n            <h3 class=\"center-align\">{{circleSession._name}}</h3>\n        </div>\n\n        <div class=\"row\" *ngIf=\"!circleSession._inProgress\">\n            <h5>Deze sessie is nog niet actief...</h5>\n        </div>\n\n        <pregame [circleSession]=\"circleSession\" *ngIf=\"circleSession._inProgress && circleSession._isPreGame\"></pregame>\n\n        <div id=\"game\" *ngIf=\"circleSession._inProgress && !circleSession._isPreGame\">\n            <div class=\"row margin-top\">\n                <div class=\"col s6 offset-s3\">\n                    <svg [attr.viewBox]=\"constants.VIEWBOX\">\n                        <!-- Draw Kandoe board circles -->\n                        <circle *ngFor=\"#filled of constants.RINGS; #i = index\"\n                                [attr.r]=\"constants.CircleRadius(i+1)\"\n                                [attr.stroke-width]=\"constants.RING_WIDTH\"\n                                [attr.cy]=\"constants.CENTER\" [attr.cx]=\"constants.CENTER\"\n                                id=\"circle-{{i+1}}\" class=\"kandoeRing\" [class.inner]=\"filled\"/>\n\n\n                        <!-- FIXME: kleur correct aanduiden, index komt niet overeen met index hieronder -->\n                        <circle *ngFor=\"#bol of pst | onBoardCards; #i = index\"\n                                [class.hoveredBall]=\"bol._cardId != null && hoveredCardId === bol._cardId\"\n                                [id]=\"bol._cardId\"\n                                [attr.r]=\"35\"\n                                [attr.fill]=\"colors.get(bol._cardId)\"\n                                [attr.cy]=\"constants.YPOS_CIRCLE(bol._position, (1 / pst.length) * i)\"\n                                [attr.cx]=\"constants.XPOS_CIRCLE(bol._position, (1 / pst.length) * i)\" />\n                    </svg>\n                </div>\n            </div>\n            <div class=\"row\">\n                <circlesession-carddetail *ngFor=\"#card of cards; #i = index\" [card]=\"card\" [color]=\"colors.get(card._id)\" (hover)=\"hover(card._id, $event)\" (playCard)=\"playCard($event)\"></circlesession-carddetail>\n            </div>\n        </div>\n\n        <user-list [currentPlayerId]=\"circleSession._currentPlayerId\" [users]=\"circleSession._userIds\" [circleSessionId]=\"circleSession._id\"></user-list>\n    </div>\n    ",
             directives: [common_1.CORE_DIRECTIVES, circleSessionCardDetail_1.CircleSessionCardDetail, circleSessionUserList_1.CircleSessionUserList, circleSessionPreGame_1.CircleSessionPreGame, common_2.NgFor],
             pipes: [circleSessionCardOnBoardPipe_1.CircleSessionCardOnBoardPipe]
-        }), 
-        __metadata('design:paramtypes', [circleSessionService_1.CircleSessionService, themeService_1.ThemeService, router_1.RouteParams])
+        }),
+        __param(2, core_1.Inject('App.SocketUrl')), 
+        __metadata('design:paramtypes', [circleSessionService_1.CircleSessionService, themeService_1.ThemeService, String, router_1.RouteParams])
     ], CircleSessionGame);
     return CircleSessionGame;
 })();
