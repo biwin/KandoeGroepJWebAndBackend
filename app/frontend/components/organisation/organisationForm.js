9c25941e2fa24302324caf40d0f8dbcb52b1e866
Michaël De Boey <info@michaeldeboey.be>
2016-03-17 21:27:34 +0100

    Show error message when creating an Organisation with an allready existing name

--- app/frontend/components/organisation/organisationForm.js
+++ app/frontend/components/organisation/organisationForm.js
@@ -1,3 +1,4 @@
+"use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -16,6 +17,7 @@ var userService_1 = require("../../services/userService");
 var OrganisationForm = (function () {
     function OrganisationForm(router, organisationService, userService) {
         this.organisation = organisation_1.Organisation.empty();
+        this.showError = false;
         this.router = router;
         this.organisationService = organisationService;
         this.userService = userService;
@@ -25,18 +27,24 @@ var OrganisationForm = (function () {
         var userId = this.userService.getUserId();
         this.organisation._organisatorIds.push(userId);
         this.organisationService.createOrganisation(this.organisation).subscribe(function (o) {
-            _this.router.navigate(["/OrganisationDetail", { id: o._id }]);
+            if (o != null) {
+                _this.router.navigate(["/OrganisationDetail", { id: o._id }]);
+            }
+            else {
+                //FIXME: unexpected end of input
+                _this.showError = true;
+            }
         });
     };
     OrganisationForm = __decorate([
         core_1.Component({
             selector: 'organisation-form',
-            template: "\n    <div class=\"row container\">\n        <h5>Maak nieuwe organisatie aan</h5>\n\n        <div class=\"card formCard\"><div class=\"card-content\">\n            <form (submit)=\"OnSubmit()\" class=\"col s12\">\n                <div class=\"row\"><div class=\"input-field col s6\">\n                    <input [(ngModel)]=\"organisation._name\" id=\"name\" type=\"text\">\n                    <label for=\"name\">Naam</label>\n                </div></div>\n\n                <button type=\"submit\" class=\"waves-effect waves-light btn red\"><i class=\"material-icons center\">add</i></button>\n            </form>\n        </div></div>\n    </div>\n    ",
+            template: "\n    <div class=\"row container\">\n        <h5>Maak nieuwe organisatie aan</h5>\n\n        <div class=\"card formCard\"><div class=\"card-content\">\n            <p *ngIf=\"showError\" class=\"error red-text\">Er bestaat reeds een organisatie met de naam {{organisation._name}}. Gelieve een andere naam te kiezen.</p>\n        \n            <form (submit)=\"OnSubmit()\" class=\"col s12\">\n                <div class=\"row\"><div class=\"input-field col s6\">\n                    <input [(ngModel)]=\"organisation._name\" id=\"name\" type=\"text\">\n                    <label for=\"name\">Naam</label>\n                </div></div>\n\n                <button type=\"submit\" class=\"waves-effect waves-light btn red\"><i class=\"material-icons center\">add</i></button>\n            </form>\n        </div></div>\n    </div>\n    ",
             directives: [common_1.CORE_DIRECTIVES, common_1.FORM_DIRECTIVES]
         }), 
         __metadata('design:paramtypes', [router_1.Router, organisationService_1.OrganisationService, userService_1.UserService])
     ], OrganisationForm);
     return OrganisationForm;
-})();
+}());
 exports.OrganisationForm = OrganisationForm;
 //# sourceMappingURL=organisationForm.js.map
