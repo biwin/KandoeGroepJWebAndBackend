ff63a48704859f2df6f46eaa9b7f3e7e15930c93
Michaël De Boey <info@michaeldeboey.be>
2016-03-18 15:28:00 +0100

    Deleted unnecessary imports

--- app/frontend/components/circleSession/circleSessionOverview.ts
+++ app/frontend/components/circleSession/circleSessionOverview.ts
@@ -1,4 +1,4 @@
-import {Component, Inject} from "angular2/core";
+import {Component} from "angular2/core";
 import {CORE_DIRECTIVES} from "angular2/common";
 import {ROUTER_DIRECTIVES} from "angular2/router";
 
